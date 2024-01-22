475823bdbb6fdf41c6b531711542cde6df909506
Paul Fitzgerald <paulfitz99@gmail.com>
2024-01-22 15:18:40 +0000

    use memo to prevent re render

--- src/status_im/contexts/shell/share/wallet/view.cljs
+++ src/status_im/contexts/shell/share/wallet/view.cljs
@@ -1,16 +1,16 @@
 (ns status-im.contexts.shell.share.wallet.view
   (:require
-    [quo.core :as quo]
-    [react-native.core :as rn]
-    [react-native.platform :as platform]
-    [react-native.share :as share]
-    [reagent.core :as reagent]
-    [status-im.contexts.shell.share.style :as style]
-    [status-im.contexts.wallet.common.sheets.network-preferences.view :as network-preferences]
-    [status-im.contexts.wallet.common.utils :as utils]
-    [utils.i18n :as i18n]
-    [utils.image-server :as image-server]
-    [utils.re-frame :as rf]))
+   [quo.core :as quo]
+   [react-native.core :as rn]
+   [react-native.platform :as platform]
+   [react-native.share :as share]
+   [reagent.core :as reagent]
+   [status-im.contexts.shell.share.style :as style]
+   [status-im.contexts.wallet.common.sheets.network-preferences.view :as network-preferences]
+   [status-im.contexts.wallet.common.utils :as utils]
+   [utils.i18n :as i18n]
+   [utils.image-server :as image-server]
+   [utils.re-frame :as rf]))
 
 (def qr-size 500)
 
@@ -48,10 +48,10 @@
   (let [selected-networks (reagent/atom [:ethereum :optimism :arbitrum])
         wallet-type       (reagent/atom :wallet-legacy)]
     (fn []
-      (let [share-title         (str (:name account) " " (i18n/label :t/address))
+      (let [share-title         (str (.-name account) " " (i18n/label :t/address))
             qr-url              (utils/get-wallet-qr {:wallet-type       @wallet-type
                                                       :selected-networks @selected-networks
-                                                      :address           (:address account)})
+                                                      :address           (.-address account)})
             qr-media-server-uri (image-server/get-qr-image-uri-for-any-url
                                  {:url         qr-url
                                   :port        (rf/sub [:mediaserver/port])
@@ -93,16 +93,14 @@
    (for [i (range n)]
      ^{:key i} [indicator (= current-index i)])])
 
-(defn- render-item
-  [_]
-  (let [width (rf/sub [:dimensions/window-width])]
-    (reagent/create-class
-     {:should-component-update
-      (fn [_ [_ old-item] [_ new-item]]
-        (not= old-item new-item))
-      :reagent-render
-      (fn [item index]
-        [wallet-qr-code-item item width index])})))
+(def render-item
+  (reagent/adapt-react-class
+   (rn/memo
+    (fn [item]
+      (print item)
+      (let [width (rf/sub [:dimensions/window-width])]
+        (reagent/as-element
+         [wallet-qr-code-item item width (.-position item)]))))))
 
 (defn wallet-tab
   []
@@ -112,20 +110,21 @@
     (fn []
       [rn/view
        [rn/flat-list
-        {:horizontal                        true
-         :deceleration-rate                 0.9
-         :snap-to-alignment                 "start"
-         :snap-to-interval                  (- width 30)
-         :disable-interval-momentum         true
-         :scroll-event-throttle             64
-         :data                              accounts
-         :directional-lock-enabled          true
+        {:horizontal true
+         :deceleration-rate 0.9
+         :snap-to-alignment "start"
+         :snap-to-interval (- width 30)
+         :disable-interval-momentum true
+         :scroll-event-throttle 64
+         :data accounts
+         :directional-lock-enabled true
          :shows-horizontal-scroll-indicator false
-         :on-scroll                         (fn [e]
-                                              (reset! current-index (js/Math.ceil
-                                                                     (/ e.nativeEvent.contentOffset.x
-                                                                        width))))
-         :render-fn                         render-item}]
+         :on-scroll (fn [e]
+                      (reset! current-index (js/Math.ceil
+                                             (/ e.nativeEvent.contentOffset.x
+                                                width))))
+         :render-fn
+         render-item}]
        [rn/view
         {:style {:margin-top 20}}
         (indicator-list (count accounts) @current-index)]])))
